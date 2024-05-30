cf3197ae599eafc542245afa6df132b379b50db8
BastD29 <bastiendaniel9@gmail.com>
2024-05-30 11:57:31 +0200

    removed some import to fix netlify production build

--- src/context/ModalContext/ModalContext.tsx
+++ src/context/ModalContext/ModalContext.tsx
@@ -1,5 +1,9 @@
-import { Context, createContext } from "react";
-import { ModalContextType } from "../../models/modal";
+import { Context, ReactNode, createContext } from "react";
+
+type ModalContextType = {
+  unsetModal: () => void;
+  setModal: (modal: ReactNode) => void;
+};
 
 export const ModalContext: Context<ModalContextType | undefined> =
   createContext<ModalContextType | undefined>(undefined);
