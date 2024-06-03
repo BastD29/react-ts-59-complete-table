abb789b71a5ad80365108602069f2361fdd37d81
BastD29 <bastiendaniel9@gmail.com>
2024-06-03 15:35:23 +0200

    added form dropdown

---
+++ src/components/Dropdown/Dropdown3.tsx
@@ -0,0 +1,83 @@
+import { FC, useMemo, useRef, useState } from "react";
+import ChevronUpIcon from "../Icons/ChevronUpIcon/ChevronUpIcon";
+import ChevronDownIcon from "../Icons/ChevronDownIcon/ChevronDownIcon";
+import useOutsideClick from "../../hooks/useOutsideClick";
+import { PlantType } from "../../models/plant";
+import { plants } from "../../data/plants";
+import style from "./Dropdown2.module.scss";
+
+type DropdownProps = {
+  label: string;
+  name: string;
+  value: string;
+  // options: OptionType[];
+  onChange: (name: string, value: string) => void;
+};
+
+// const getUniqueOptions = (arr: PlantType[], key: keyof PlantType) => {
+//   return Array.from(new Set(arr.map((item) => String(item[key]))));
+// };
+
+const getUniqueOptions = (attribute: keyof PlantType) => {
+  return Array.from(new Set(plants.map((plant) => plant[attribute]))).map(
+    (value) => ({
+      value: value as string,
+      label: value as string,
+    })
+  );
+};
+
+const Dropdown: FC<DropdownProps> = ({
+  label,
+  name,
+  onChange,
+  // options,
+  value,
+}) => {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+
+  const handleOptionClick = (optionValue: string) => {
+    onChange(name, optionValue);
+    setIsOpen(false);
+  };
+
+  const dropdownRef = useRef<HTMLDivElement>(null);
+
+  useOutsideClick(dropdownRef, () => {
+    if (isOpen) setIsOpen(false);
+  });
+
+  const options = useMemo(() => getUniqueOptions(name as keyof PlantType), []);
+
+  return (
+    <div className={style["dropdown"]} ref={dropdownRef}>
+      <label className={style["dropdown__label"]}>
+        {label}
+        <div
+          className={style["dropdown__header"]}
+          onClick={() => setIsOpen(!isOpen)}
+        >
+          {value
+            ? options.find((option) => option.value === value)?.label
+            : `Select ${label}`}
+          {isOpen ? <ChevronUpIcon /> : <ChevronDownIcon />}
+        </div>
+        {isOpen && (
+          <div className={style["dropdown__list"]}>
+            {options.map((option) => (
+              <div
+                key={option.value}
+                className={style["dropdown__option"]}
+                onClick={() => handleOptionClick(option.value)}
+              >
+                {option.label}
+              </div>
+            ))}
+          </div>
+        )}
+      </label>
+    </div>
+  );
+};
+
+export default Dropdown;
---
+++ src/components/Form/Form3.tsx
@@ -0,0 +1,87 @@
+import { /* ChangeEvent, */ FormEvent, useState } from "react";
+import { useRowsContext } from "../../hooks/contexts/useRowsContext";
+import { useModalContext } from "../../hooks/contexts/useModalContext";
+import { PlantType } from "../../models/plant";
+import { ADD_ROW, UNSET_MODAL } from "../../constants/actions";
+import Dropdown from "../Dropdown/Dropdown3";
+import style from "./Form.module.scss";
+
+const Form: React.FC = () => {
+  const { dispatch: rowDispatch, state: rows } = useRowsContext();
+  const { dispatch: modalDispatch } = useModalContext();
+
+  const initialFormValues: Omit<PlantType, "id"> = {
+    name: "",
+    cycle: "",
+    sunlight: "",
+    watering: "",
+  };
+
+  const [row, setRow] = useState<Omit<PlantType, "id">>(initialFormValues);
+  const [error, setError] = useState<string>("");
+
+  const handleInputChange = (name: string, value: string) => {
+    setRow({ ...row, [name]: value });
+  };
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (!row.name || !row.cycle || !row.sunlight || !row.watering) {
+      setError("All fields are required.");
+      return;
+    }
+    const newId = rows.length ? Math.max(...rows.map((r) => r.id)) + 1 : 1;
+    const newRow: PlantType = {
+      id: newId,
+      ...row,
+    };
+    rowDispatch({ type: ADD_ROW, payload: newRow });
+    modalDispatch({ type: UNSET_MODAL });
+    // console.log("submitted");
+    setRow(initialFormValues);
+  };
+
+  return (
+    <form className={style["form"]} onSubmit={handleSubmit}>
+      {error && <p className={style["form__error"]}>{error}</p>}
+      <div>
+        <label className={style["form__group"]}>
+          Name
+          <input
+            type="text"
+            name="name"
+            onChange={(e) => handleInputChange(e.target.name, e.target.value)}
+            value={row.name}
+          />
+        </label>
+      </div>
+
+      <Dropdown
+        label="Cycle"
+        name="cycle"
+        value={row.cycle}
+        onChange={handleInputChange}
+      />
+
+      <Dropdown
+        label="Sunlight"
+        name="sunlight"
+        value={row.sunlight}
+        onChange={handleInputChange}
+      />
+
+      <Dropdown
+        label="Watering"
+        name="watering"
+        value={row.watering}
+        onChange={handleInputChange}
+      />
+
+      <button className={style["form__button"]} type="submit">
+        Add
+      </button>
+    </form>
+  );
+};
+
+export default Form;
