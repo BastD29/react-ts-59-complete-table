477097b1660484e55f0fc9055d4d05fe193f0f6d
BastD29 <bastiendaniel9@gmail.com>
2024-06-04 19:47:59 +0200

    added plant name in delete modal confirm

--- src/components/Modal/ModalConfirm/ModalConfirm.tsx
+++ src/components/Modal/ModalConfirm/ModalConfirm.tsx
@@ -1,4 +1,4 @@
-import { FC } from "react";
+import { FC, useEffect, useState } from "react";
 import { useModalContext } from "../../../hooks/contexts/useModalContext";
 import { DELETE_ROW, UNSET_MODAL } from "../../../constants/actions";
 import { useRowsContext } from "../../../hooks/contexts/useRowsContext";
@@ -10,7 +10,16 @@ type ModalConfirmProps = {
 
 const ModalConfirm: FC<ModalConfirmProps> = ({ rowId }) => {
   const { dispatch: modalDispatch } = useModalContext();
-  const { dispatch: rowDispatch } = useRowsContext();
+  const { dispatch: rowDispatch, state: rows } = useRowsContext();
+
+  const [plantName, setPlantName] = useState<string>("");
+
+  useEffect(() => {
+    const plant = rows.find((row) => row.id === rowId);
+    if (plant) {
+      setPlantName(plant.name);
+    }
+  }, [rowId, rows]);
 
   const handleDelete = () => {
     rowDispatch({ type: DELETE_ROW, payload: rowId });
@@ -20,7 +29,7 @@ const ModalConfirm: FC<ModalConfirmProps> = ({ rowId }) => {
   return (
     <div className={style["modal-confirm"]}>
       <h3>Delete plant</h3>
-      <p>Are you sure you want to delete the plant?</p>
+      <p>Are you sure you want to delete the {plantName} plant?</p>
       <div className={style["modal-confirm__buttons"]}>
         <button onClick={() => modalDispatch({ type: UNSET_MODAL })}>
           Cancel
